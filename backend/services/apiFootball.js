08868d6fd19d5f7159eac2551decb3858c1679b2
roseanepfigueiredo <roseanepfigueiredo@gmail.com>
2026-04-27 17:58:09 -0300

    corrigindo api direta

--- backend/services/apiFootball.js
+++ backend/services/apiFootball.js
@@ -2,11 +2,10 @@ const axios = require('axios');
 
 async function getLiveMatches() {
   const response = await axios.get(
-    'https://api-football-v1.p.rapidapi.com/v3/fixtures?live=all',
+    'https://v3.football.api-sports.io/fixtures?live=all',
     {
       headers: {
-        'x-rapidapi-key': process.env.API_KEY,
-        'x-rapidapi-host': 'api-football-v1.p.rapidapi.com'
+        'x-apisports-key': process.env.API_KEY
       }
     }
   );
