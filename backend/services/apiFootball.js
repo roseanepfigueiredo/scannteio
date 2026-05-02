ab9931f7dbcbc2ef539da5fbacc2d2ca02ba5957
roseanepfigueiredo <roseanepfigueiredo@gmail.com>
2026-05-02 15:51:49 -0300

    teste jogos do dia

--- backend/services/apiFootball.js
+++ backend/services/apiFootball.js
@@ -3,16 +3,23 @@ const axios = require('axios');
 const API_URL = 'https://v3.football.api-sports.io';
 
 async function getLiveMatches() {
-  const response = await axios.get(`${API_URL}/fixtures?live=all`, {
-    headers: {
-      'x-apisports-key': process.env.API_KEY
+
+  const today = new Date().toISOString().split('T')[0];
+
+  const response = await axios.get(
+    `${API_URL}/fixtures?date=${today}`,
+    {
+      headers: {
+        'x-apisports-key': process.env.API_KEY
+      }
     }
-  });
+  );
 
   return response.data.response;
 }
 
 async function getMatchStatistics(fixtureId) {
+
   const response = await axios.get(
     `${API_URL}/fixtures/statistics?fixture=${fixtureId}`,
     {
@@ -25,4 +32,7 @@ async function getMatchStatistics(fixtureId) {
   return response.data.response;
 }
 
-module.exports = { getLiveMatches, getMatchStatistics };
+module.exports = {
+  getLiveMatches,
+  getMatchStatistics
+};
